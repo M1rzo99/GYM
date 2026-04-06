33dbd5e0a20ef72ad031a3190737fe152c4eda76
Mirzo <accaunoff99@gmail.com>
2026-04-06 22:03:34 +0900

    feat:making start GYM

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { Button } from '@/components/ui/button'
-
 export default function App() {
 	return (
-		<div className='flex h-screen w-full flex-col items-center justify-center gap-4'>
-			<h1 className='text-2xl'>This is app pages</h1>
-			<Button variant='link'>Button</Button>
+		<div className='flex h-screen items-center justify-center flex-col gap-4'>
+			<h1 className='text-2xl font-bold'>This is main page</h1>
+			<p className='text-center'>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. At aliquam
+				iste obcaecati quos repellendus dolor alias, tenetur in cum adipisci
+				nostrum animi dolores voluptate voluptates similique est saepe
+				necessitatibus doloremque nam. Voluptate asperiores, porro nihil,
+				incidunt voluptates doloribus dolorum, sunt enim provident iste eligendi
+				quisquam consequuntur ratione mollitia vel! Quas!
+			</p>
 		</div>
 	)
 }
