a3f834bdee1019d2fb9ff6f91a31b9ec57831eee
Mirzo <accaunoff99@gmail.com>
2026-04-22 23:13:12 +0900

    feat:added auth

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,14 +1,16 @@
-
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9zCr2COPoXdqZUqM8XQR7uzX36UzTtFE",
-  authDomain: "gym-timer-860d9.firebaseapp.com",
-  projectId: "gym-timer-860d9",
-  storageBucket: "gym-timer-860d9.firebasestorage.app",
-  messagingSenderId: "997346826921",
-  appId: "1:997346826921:web:597c2d50971eec101aa7b8"
+
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { auth };
